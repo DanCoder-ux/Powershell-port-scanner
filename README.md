6dd6f82359340a6d592bc34aef61f8f6860f9039
DanCoder-ux <dtesfaye164@gmail.com>
2024-05-07 19:40:15 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# PowerShell-Port-Scanner
+
+Made by powershell
+
+## Usage
+
+```sh
+portscanner.ps1 <target>
+```
+example
+
+```sh
+portscanner.ps1 192.168.1.1
+```
---
+++ portscanner.ps1
@@ -0,0 +1,22 @@
+param(
+	[string]$target_ip = $args[0]
+)
+
+if ( -not $target_ip) {
+	Write-Host "`n[i] Usage: .\portscanner.ps1 <target_ip>`n"
+	Exit 1
+}
+
+foreach ($port in 20.. 6000) {
+	$c = New-Object System.Net.Sockets.TcpClient
+	$a = $c.BeginConnect($target_ip, $port, $null, $null)
+	$r = $a.AsyncWaitHandle.WaitOne(250)
+
+	if ($r) {
+		Write-Host -ForegroundColor Cyan "`n[ * ] Port $port/tcp is open!`n"
+	} else {
+		Write-Host -ForegroundColor Red "[!] Port $port/tcp is closed"
+	}
+	
+	$c.Close()
+}
